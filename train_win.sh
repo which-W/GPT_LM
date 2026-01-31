80b59dc91d5aa6329c396c37368707e72d077b90
which-W <wengzu.love@gmail.com>
2026-01-31 17:04:36 +0800

    train_win push

---
+++ train_win.sh
@@ -0,0 +1,7 @@
+python train.py \
+  --train_data_path ./data/TinyStories-train.bin \
+  --valid_data_path ./data/TinyStories-valid.bin \
+  --batch_size 32 \
+  --use_wandb \
+  --wandb_project "tinystories-transformer" \
+  --wandb_run_name "fourth-experiment"
